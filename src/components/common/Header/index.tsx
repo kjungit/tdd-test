e7a0a6bcceb814bc5fff3cf723338e11964ec6de
kjun <kjun@gooroomee.com>
2024-08-14 18:21:45 +0900

    Feat: Header 컴포넌트

---
+++ src/components/common/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="w-full flex text-white h-10 items-center justify-center">
+      TDD TodoList
+    </div>
+  );
+}
