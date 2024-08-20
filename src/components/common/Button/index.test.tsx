97ddbbb79d2effb4188da05d23fd9da3e0cc9749
kjun <kjun@gooroomee.com>
2024-08-20 15:33:07 +0900

    Feat: 버튼 컴포넌트 테스트코드 수정

--- src/components/common/Button/index.test.tsx
+++ src/components/common/Button/index.test.tsx
@@ -6,7 +6,7 @@ import Button from ".";
 
 describe("Button Component", () => {
   it("기본 props(size, color)가 적용된다.", () => {
-    render(<Button cyId="" label="Click me" />);
+    render(<Button label="Click me" />);
     const buttonElement = screen.getByText("Click me");
     expect(buttonElement).toBeInTheDocument();
     expect(buttonElement).toHaveClass(
@@ -15,7 +15,7 @@ describe("Button Component", () => {
   });
 
   it("버튼의 label값이 적용되어 렌더링 된다.", () => {
-    render(<Button cyId="" label="Submit" />);
+    render(<Button label="Submit" />);
     const buttonElement = screen.getByRole("button", {
       name: "Submit",
     });
@@ -23,19 +23,19 @@ describe("Button Component", () => {
   });
 
   it("버튼의 size props별(sm, md, lg)값이 적용되어 렌더링 된다.", () => {
-    render(<Button cyId="" label="Small" size="sm" />);
+    render(<Button label="Small" size="sm" />);
     let buttonElement = screen.getByRole("button", {
       name: "Small",
     });
     expect(buttonElement).toHaveClass("w-14 h-10 rounded-md");
 
-    render(<Button cyId="" label="Medium" size="md" />);
+    render(<Button label="Medium" size="md" />);
     buttonElement = screen.getByRole("button", {
       name: "Medium",
     });
     expect(buttonElement).toHaveClass("w-16 h-12 rounded-md");
 
-    render(<Button cyId="" label="Large" size="lg" />);
+    render(<Button label="Large" size="lg" />);
     buttonElement = screen.getByRole("button", {
       name: "Large",
     });
@@ -43,13 +43,13 @@ describe("Button Component", () => {
   });
 
   it("버튼의 color props(white, gray)가 적용되어 렌더링 된다.", () => {
-    render(<Button cyId="" label="White Button" color="white" />);
+    render(<Button label="White Button" color="white" />);
     let buttonElement = screen.getByRole("button", {
       name: "White Button",
     });
     expect(buttonElement).toHaveClass("bg-white text-black hover:bg-gray-300");
 
-    render(<Button cyId="" label="Gray Button" color="gray" />);
+    render(<Button label="Gray Button" color="gray" />);
     buttonElement = screen.getByRole("button", {
       name: "Gray Button",
     });
@@ -60,7 +60,7 @@ describe("Button Component", () => {
 
   it("버튼을 클릭하면 onClick 이벤트가 호출된다.", () => {
     const handleClick = jest.fn();
-    render(<Button cyId="" label="Click me" onClick={handleClick} />);
+    render(<Button label="Click me" onClick={handleClick} />);
     const buttonElement = screen.getByRole("button", {
       name: "Click me",
     });
