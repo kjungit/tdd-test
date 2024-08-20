9bc59acef2b0d717d102c1270e6890a8426c27bd
kjun <kjun@gooroomee.com>
2024-08-20 15:33:40 +0900

    Feat: 헤더 컴포넌트 테스트코드 수정

--- src/components/common/Header/index.test.tsx
+++ src/components/common/Header/index.test.tsx
@@ -4,7 +4,24 @@
 import { render, screen } from "@testing-library/react";
 import Header from ".";
 
-it("App Router: Works with Server Components", () => {
-  render(<Header />);
-  expect(screen.getByText("TodoList")).toBeInTheDocument();
+describe("Header Component", () => {
+  it("'TDD ToDoList'라는 제목을 표시해야 합니다", () => {
+    render(<Header />);
+
+    const heading = screen.getByRole("heading", {
+      name: "TDD TodoList",
+      level: 2,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+  it("로그인 버튼이 표시되야 합니다.", () => {
+    render(<Header />);
+
+    const loginButton = screen.getByRole("button", {
+      name: "로그인",
+    });
+
+    expect(loginButton).toBeInTheDocument();
+  });
 });
