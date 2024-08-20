88502cd0f31a5b7df3b25c499f6ea3ad0211d9de
kjun <kjun@gooroomee.com>
2024-08-20 15:33:59 +0900

    Feat: 헤더 컴포넌트 버튼 추가(테스트)

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Button from "../Button";
 
 export default function Header() {
   return (
-    <div className="w-full flex text-white h-10 items-center justify-center">
-      TDD TodoList
+    <div className="flex justify-between w-full h-20 items-center">
+      <h2 className="w-full flex text-white h-10 items-center justify-center">
+        TDD TodoList
+      </h2>
+      <Button label="로그인" />
     </div>
   );
 }
